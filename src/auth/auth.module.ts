0480baf142e4de48f6cafde0fea23a1d4f91bc89
ckdwns9121 <ckdwns9121@naver.com>
2023-01-24 15:38:41 +0900

    # TypeOrm 관계 설정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { User } from './user.entity';
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
-      secret: 'Secret:1234',
+      secret: 'Secret1234',
       signOptions: {
         expiresIn: 3600,
       },
